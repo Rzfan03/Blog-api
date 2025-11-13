0a34fb35edc626eb6bb4dec9d3cc4cffd80e3208
rzfann <fajrulsyaban47@gmail.com>
2025-11-13 09:34:48 +0700

    SEMOGA BISA!

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 
 const SUPABASE_URL = 'https://npnhsysnwlimuoyijorm.supabase.co';
-const SUPABASE_KEY = 'YOUR_KEY_HERE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wbmhzeXNud2xpbXVveWlqb3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5OTMzMDQsImV4cCI6MjA3ODU2OTMwNH0.ImUGL4NxqeO2KPPUk-ZwTrsJ2UcSK-sTgYCg_yaNjck';
+
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 app.get("/", async (req, res) => {
